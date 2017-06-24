0e12520448a128ed1b076ef573af517c9f239318
Duncan Leo <duncanleo97@gmail.com>
2017-06-24 12:24:28 +0800

    Add YouTube code

---
+++ vendor/youtube/youtube.go
@@ -0,0 +1,40 @@
+package youtube
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+// Search for a Youtube Video
+func Search(term string, numResults int) ([]YoutubeVideo, error) {
+	var results []YoutubeVideo
+	cmd := exec.Command("youtube-dl", fmt.Sprintf("ytsearch%d:%s", numResults, term), "-f", "bestaudio", "--skip-download", "--no-playlist", "--print-json")
+
+	var outBuffer, errBuffer bytes.Buffer
+	cmd.Stdout = &outBuffer
+	cmd.Stderr = &errBuffer
+
+	err := cmd.Run()
+	if err != nil {
+		return results, err
+	}
+
+	if outBuffer.Len() == 0 {
+		return results, errors.New(errBuffer.String())
+	}
+
+	lines := strings.Split(outBuffer.String(), "\n")
+	for _, line := range lines {
+		var result YoutubeVideo
+		err := json.Unmarshal([]byte(line), &result)
+		if err == nil {
+			results = append(results, result)
+		}
+	}
+
+	return results, nil
+}
