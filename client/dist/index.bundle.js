a9c7ccc1276f4688cd2c764afa457586700e5880
Duncan Leo <duncanleo97@gmail.com>
2017-06-24 10:01:48 +0800

    Add textual websockets

--- client/dist/index.bundle.js
+++ client/dist/index.bundle.js
@@ -1,1 +1,10 @@
-console.log('Oh my god!');
+const socket = new WebSocket("ws://localhost:8080/stream", "GET");
+
+socket.onmessage = (event) => {
+  console.log(event.data);
+};
+
+socket.onopen = (event) => {
+  socket.send('Hello, this is JS!');
+};
+
