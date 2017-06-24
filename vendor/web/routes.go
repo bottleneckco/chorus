a0493cdc9679c79e63965befa9c5b201242447ba
Ivan Tan <ivantjh97@gmail.com>
2017-06-25 04:19:12 +0800

    Use gin-cors to handle OPTIONS method from all routes

--- vendor/web/routes.go
+++ vendor/web/routes.go
@@ -6,16 +6,10 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/itsjamie/gin-cors"
 )
 
 var channelMap = make(map[ChannelID]Channel)
-
-func preflight(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", "POST, OPTIONS")
-	c.Header("Access-Control-Allow-Headers", "Content-Type")
-	c.AbortWithStatus(200)
-}
 
 // StartServer yay
 func StartServer() {
@@ -29,6 +23,15 @@ func StartServer() {
 		},
 	)
 
+	router.Use(cors.Middleware(cors.Config{
+		Origins:         "*",
+		Methods:         "GET, PUT, POST, DELETE",
+		RequestHeaders:  "Origin, Content-Type",
+		ExposedHeaders:  "",
+		Credentials:     true,
+		ValidateHeaders: false,
+	}))
+
 	apiR := router.Group("/api")
 	{
 		channelR := apiR.Group("/channels")
@@ -36,7 +39,6 @@ func StartServer() {
 			channelR.GET("/:id/search", searchMusic)
 
 			channelR.GET("/:id", getChannel)
-			channelR.OPTIONS("", preflight)
 			channelR.POST("", createChannel)
 
 			channelR.GET("/:id/queue", getChannelQueue)
