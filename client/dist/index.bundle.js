4fec498603ac41f99511a92e7f37995a0b429a27
Duncan Leo <duncanleo97@gmail.com>
2017-06-24 10:40:05 +0800

    use Promises

--- client/dist/index.bundle.js
+++ client/dist/index.bundle.js
@@ -3,18 +3,26 @@ const socket = new WebSocket("ws://localhost:8080/stream", "GET");
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const context = new AudioContext();
 
+const convertToArrayBuffer = (data) => {
+  return new Promise((resolve) => {
+    const fileReader = new FileReader();
+    fileReader.onload = function () {
+      resolve(this.result);
+    };
+    fileReader.readAsArrayBuffer(data);
+  });
+}
+
 socket.onmessage = (event) => {
   const source = context.createBufferSource();
 
-  const fileReader = new FileReader();
-  fileReader.onload = function () {
-    context.decodeAudioData(this.result, (buffer) => {
+  convertToArrayBuffer(event.data).then((arrayBuffer) => {
+    context.decodeAudioData(arrayBuffer, (buffer) => {
       source.buffer = buffer;
       source.connect(context.destination);
       source.start(context.currentTime);
     });
-  };
-  fileReader.readAsArrayBuffer(event.data);
+  });
 };
 
 socket.onopen = (event) => {
