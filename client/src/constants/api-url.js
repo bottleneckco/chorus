b9178417d3fadacad0e3b3269ca9eb366aacab43
Duncan Leo <duncanleo97@gmail.com>
2017-06-26 03:49:37 +0800

    fix stray colon

--- client/src/constants/api-url.js
+++ client/src/constants/api-url.js
@@ -1,2 +1,2 @@
-export const WS_ROOT = `${window.location.protocol.replace('http', 'ws')}://${window.location.host}`;
+export const WS_ROOT = `${window.location.protocol.replace('http', 'ws')}//${window.location.host}`;
 export default `${window.location.protocol}//${window.location.host}`;
